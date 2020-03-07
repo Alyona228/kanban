214deb7de4cae1b8e50b8d04061e7ebdf99847a1
Name <56693963+Alyona228@users.noreply.github.com>
2020-03-07 21:11:36 +0300

    Style fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,11 @@ function Footer(params) {
   console.log(params)
   return (
     <div className='footer'>
-      <div>Active tasks:</div>
-      <div>Finished tasks:</div>
-      <div>Kanban board by </div>
+      <div className='footer__left'>Active tasks: &#8249;N&#8250; </div>
+      <div className='footer__centr'>Finished tasks: &#8249;M&#8250; </div>
+      <div className='footer__right'>
+        Kanban board by &#8249;NAME&#8250;, &#8249;YEAR&#8250;{' '}
+      </div>
     </div>
   )
 }
