fd223d80308017d332e36a10b11b3318d3faea51
Name <56693963+Alyona228@users.noreply.github.com>
2020-03-28 13:59:23 +0300

    Input

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,95 +1,49 @@
 import React from 'react'
 import '../styles/Board.css'
-import menuButton from '../images/1.svg'
-import Item from './Item'
-import '../styles/Item.css'
-import CreateItem from './CreateItem'
+// import menuButton from '../images/1.svg'
+// import Item from './Item'
+// import '../styles/Item.css'
+// import CreateItem from './CreateItem'
 
 export default class Board extends React.Component {
   constructor() {
     super()
     this.state = {
-      newItem: null,
+      showInput: false,
+      flag: true,
     }
   }
-  addItemAnyword() {
-    if (this.state.newItem === null) {
-      this.setState({
-        newItem: 'text',
-      })
-    }
-  }
-
-  removeItemAnyword() {
+  handleClickAdd = () => {
     this.setState({
-      newItem: null,
+      showInput: true,
     })
   }
-
-  changeNewItem(text) {
+  handleBlur =()=>{
     this.setState({
-      newItem: text,
+      flag:
     })
-  }
-
-  createNewItem() {
-    if (this.state.newItem !== null) {
-      this.props.addItem(this.state.newItem)
-      this.setState({
-        newItem: null,
-      })
-    }
   }
 
   render() {
-    let items = this.props.items.map((item, index) => {
-      return (
-        <Item
-          removeItem={this.props.removeItem.bind(this, index)}
-          name={item}
-          key={index}
-        />
-      )
-    })
-    let addItemElement
-    if (this.props.itemsToAdd() === null) {
-      addItemElement =
-        this.state.newItem !== null ? (
-          <Item
-            createFn={this.createNewItem.bind(this)}
-            changeFn={this.changeNewItem.bind(this)}
-            text={this.state.item}
-          ></Item>
-        ) : (
-          ''
-        )
-    } else {
-      addItemElement =
-        this.state.newItem === null ? (
-          ''
-        ) : (
-          <select>
-            {this.props.itemsToAdd().map((item, index) => (
-              <option key={index}>{item}</option>
-            ))}
-          </select>
-        )
-    }
     return (
       <div className='board'>
-        <div className='backlog'>
-          {this.props.name}
-          <img src={menuButton} alt='это аватар' className='img' />
-        </div>
-        <CreateItem> {this.props.addItemElement} </CreateItem>
+        <div className='board-title'>{this.props.title}</div>
+        {this.state.showInput ? (
+          <input className='new-task-input' onBlur={this.handleBlur}/>
+        ) : (
+          undefined
+        )}
         <ul className='list'>
-          <button
-            className='buttons__button buttons__button--add'
-            onClick={this.addItemAnyword.bind(this)}
-          >
-            +Add card
-          </button>
+          {this.props.tasks.map((item, ind) => (
+            <li key={ind}>{item}</li>
+          ))}
         </ul>
+        <button
+          className='buttons__button buttons__button--add'
+          onClick={this.handleClickAdd}
+        >
+          +Add card
+        </button>
       </div>
     )
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,67 +5,35 @@ import Board from './Board.js'
 export default class Main extends React.Component {
   constructor() {
     super()
-
     this.state = {
-      boards: [
-        {
-          name: 'Backlog',
-          items: ['Login page - performance issues', '', ''],
-        },
-        {
-          name: 'Ready',
-          items: ['', '', '', ''],
-        },
-        {
-          name: 'In Progress',
-          items: ['', '', ''],
-        },
-        {
-          name: 'Finished',
-          items: ['', '', ''],
-        },
-      ],
+      tasks: [''],
     }
   }
-
-  addNewItem(index, item) {
-    let newBoards = this.state.boards
-    let board = newBoards[index]
-    board.items.push(item)
-    this.setState({
-      boards: newBoards,
-    })
-  }
-
-  removeItem(boardIndex, itemIndex) {
-    let newBoards = this.state.boards
-    let board = newBoards[boardIndex]
-    board.items.splice(itemIndex, 1)
-    this.setState({
-      boards: newBoards,
-    })
-  }
-
-  getPreviousBoardItems(boardIndex) {
-    if (boardIndex > 0) {
-      return this.state.boards[boardIndex - 1].items
-    }
-    return null
-  }
-
+  handleAddTask = () => {}
   render() {
-    const boards = this.state.boards.map((board, index) => {
-      return (
+    return (
+      <div className='body__boards'>
         <Board
-          itemsToAdd={this.getPreviousBoardItems.bind(this, index)}
-          name={board.name}
-          items={board.items}
-          addItem={this.addNewItem.bind(this, index)}
-          removeItem={this.removeItem.bind(this, index)}
-          key={index}
-        ></Board>
-      )
-    })
-    return <div className='body__boards'>{boards}</div>
+          title='Backlog'
+          tasks={this.state.tasks}
+          handleAddTask={this.handleAddTask}
+        />
+        {/* <Board
+          title='Ready'
+          tasks={this.state.tasks}
+          handleAddTask={this.handleAddTask}
+        />
+        <Board
+          title='In Progress'
+          tasks={this.state.tasks}
+          handleAddTask={this.handleAddTask}
+        />
+        <Board
+          title='Finished'
+          tasks={this.state.tasks}
+          handleAddTask={this.handleAddTask} */}
+        />
+      </div>
+    )
   }
 }
